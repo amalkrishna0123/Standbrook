68a713febf129c34ac6bb9fac0a76e57b93d597f
amalkrishna0123 <amalikka0@gmail.com>
2024-06-01 12:37:42 +0530

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xxs': '320px',
+        'xs': '375px',
+        'xsm':'425px',     
+        'sm': '540px',     
+        'xmd': '680px',     
+        'md': '768px',    
+        'xxl': '900px',    
+        'xl': '1024px',   
+        '2xl': '1200px',
+        '3xl':'1440px',   
+      },
+    },
   },
   plugins: [],
 }
